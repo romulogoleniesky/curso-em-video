6b8600c2b4e341fbe8960892ba313bcd37371ea5
romulogoleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2020-05-26 15:57:57 -0300

    primeira commit do python

---
+++ C_E_V_Python/Funções.py
@@ -0,0 +1,21 @@
+# CURSO EM VÍDEO - CURSO DE PYTHON - AULA 20 - FUNÇÕES.
+
+# EXPLIACAÇÃO
+# Função é um comando que pode ser criado quando há a necessidade do
+# mesmo ser repetido várias vezes.
+#
+# DEF : È o comando que DEFINE uma FUNÇÃO:
+# MENSG : É o nome que foi atibuído á FUNÇÃO.
+# MSG : É o nome do parâmetro que vai ser alterado conforme a necessidade.
+#
+# EXEMPLO:
+
+
+def mensg(msg):
+    print("-----------------------------------")
+    print(msg)
+    print("-----------------------------------")
+
+
+mensg("Eu sou aluno");
+mensg("Meu nome é Rômulo");
