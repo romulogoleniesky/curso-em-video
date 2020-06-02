66166d722dd2868c2f14ae4b3be970899dad01c0
romulogoleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2020-06-01 23:34:23 -0300

    desafio de python 001,002.

---
+++ C_E_V_Python/Desafios/desafio_001_print.py
@@ -0,0 +1,10 @@
+# DESAFIO 001 - PRINT - AULA 04;
+# Faça um programa que imprima um texto na tela,(Hello Word!):
+
+
+print("Hello Word!")
+
+# ou assim:
+
+texto = "Hello Word!"
+print(texto)
---
+++ C_E_V_Python/Desafios/desafio_002_input.py
@@ -0,0 +1,17 @@
+# DESAFIO 002 - INPUT - AULA 04:
+# Faça um programa que leia o nome do usuário e mostre uma mensagem de boas vindas.
+
+nome = input("Olá, qual é o seu nome? ")
+print("Seja bem vindo "+nome+"!")
+
+# de outra maneira;
+
+nome2 = input("Oi! Pode me dizer o seu nome? ")
+print("Seja bem vindo {} !".format(nome2))
+
+# outra maneira;
+
+nome3 = input("Oi amigo, como você se chama? ")
+print(f'{nome3} é um nome muito bonito!')
+
+#fim.
