4ba305d4cb704a9f148bcbac46d8acd3581e4500
romulogoleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2020-06-03 22:56:31 -0300

    Create aula07ex001.js

---
+++ JAVASCRIPT_CEV/aula_07/aula07ex001.js
@@ -0,0 +1,55 @@
+// Aula 07 - Operadores aritimeticos.
+
+//  Soma (+)
+//  Subtração (-)
+//  Divisão (/)
+//  Multiplicação (*)
+//  Resto da divisão (%)
+//  Potência   (**)
+
+// Ordem de precedência:
+// >> Primeiro:  ()
+// >> Segundo :  **
+// >> Terceiro: * , / , %
+// >> Último : 
+
+// EXEMPLOS:
+var n1 = 10
+var n2 = 5
+var soma = (n1+n2)
+var Subtração = (n1-n2)
+var Divisão = (n1/n2)
+var Multiplicação = (n1*n2)
+var Resto da Divisão= (n1%n2)
+var Potência = (n1**n2)
+
+//ATRIBUÍÇÕES NORMAIS:
+var n = 3
+n = n + 4
+n = n - 4
+n = n * 4
+n = n / 4
+n = n ** 4
+n = n % 4
+
+//ATRIBUÍÇÕES SIMPLIFICADAS:
+n += 4
+n -= 4
+n *= 4
+n /= 4
+n **= 4
+n %= 4
+
+//INCREMENTO:
+//normal:
+var x = 5
+x = x + 1
+x = x - 1
+//simples:
+x += 1
+x -= 1
+//simplificado:
+x ++ 1
+x -- 1
+
+//fim
