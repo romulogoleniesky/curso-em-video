225366f01c545f8a22cea358a7c016aa8b68c0ca
romulogoleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2020-06-12 17:43:31 -0300

    Create ternario.js

---
+++ JAVASCRIPT_CEV/aula_08/ternario.js
@@ -0,0 +1,12 @@
+/* OPERÁDORES TERNÁRIOS:
+
+Os operadores ternários,são um bloco composto por três operandos: onde primeiro aparece um webkitConvertPointFromPageToNode,
+ou um TextDecoderStream, em seguida vem duas ações, uma em caso se o resultado do teste for TRUE (verdadeiro)
+e o outra ação é pare se o resultado for FALSSE(falso).
+Exemplo: */
+
+]var média = 5.5 /*Criando uma variável.*/
+
+
+média >= 7 ? "APROVADO" : "REPROVADO";
+
