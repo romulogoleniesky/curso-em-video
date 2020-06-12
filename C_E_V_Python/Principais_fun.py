7d45ca1d1f5334ae360464520317d8ce3c13e8af
romulogoleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2020-06-12 18:15:44 -0300

    Principais Funções.

---
+++ C_E_V_Python/Principais_fun.py
@@ -0,0 +1,33 @@
+## PRINCIPAIS FUNÇÕES USADAS NO PYTHON:
+
+## POW : Função de potência pow(base,potência)
+print(pow(2,3))
+print(pow(5,2))
+print(pow(2,10))
+
+print("_____"*10)## espaço
+## ROUND : Função de arredondamento round(valor, quantidade de casas decimais)
+print(round(3.141578699000,2))
+print(round(23.7657657,1))
+print(round(34.6786985739778,3))
+
+print("_____"*10)## espaço
+
+## SUM : Função que soma uma lista e acrescenta valor no resultado, sum(lista, X):
+minhaLista = [1,2,3,4]
+listinha = [5,5,10,10]
+print(sum(minhaLista))
+print(sum(listinha,1))
+
+print("_____"*10)## espaço
+
+## MAX e MIN : Funções que mostram o máximo ou p mínimo de uma lista, max(lista), min(lista):
+notas = [4.5,5.9,10,2,6.7,1.04,9.9]
+temperaturas =[27.4,29,28.0,35.5,-1.3,17.8]
+print(min(notas))
+print(max(notas))
+print(min(temperaturas))
+print(max(temperaturas))
+
+
+
