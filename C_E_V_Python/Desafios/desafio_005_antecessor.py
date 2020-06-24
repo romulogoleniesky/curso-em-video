9cae1d51fbd6dcde1691b472dd3a2503eb3b930f
romulogoleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2020-06-23 21:47:19 -0300

    Desafio 005.

---
+++ C_E_V_Python/Desafios/desafio_005_antecessor.py
@@ -0,0 +1,11 @@
+# DESAFIO - 005 - ANTECESSOR E SUCESSOR:
+# Escreva um código em que peça um número ao usuário e retorne esse número
+# mais o seu antecessor e seu sucessor:
+
+numero = int(input("Digite um número: "))
+antecessor = numero - 1
+sucessor = numero + 1
+print(f"Você digitou {numero} , seu antecessor é {antecessor} e seu sucessor é {sucessor} ! ")
+
+n = int(input("Digite um número: "))
+print(f"Você digitou {n} , seu antecessor é {n - 1} e seu sucessor é {n + 1} ! ")
