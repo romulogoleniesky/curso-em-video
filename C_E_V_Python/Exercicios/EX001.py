eb8d025e3766ae978d8eb365649abdf6edfaa193
RomuloGoleniesky <goleniesky@hotmail.com>
2021-04-04 00:12:42 -0300

    Criado exercicios
    Criado o EX001 e EX002

---
+++ C_E_V_Python/Exercicios/EX001.py
@@ -0,0 +1,9 @@
+## Crie um programa que escreva "Olá Mundo!" na tela:
+
+##primeiro modo
+print("Olá mundo!")
+
+##segunda maneira:
+saudacao= "Olá Mundo !"
+print(saudacao)
+
---
+++ C_E_V_Python/Exercicios/EX002.py
@@ -0,0 +1,18 @@
+## Crie um programa onde peça o nome:
+
+
+## Variável nome criada.
+nome = input("Olá, qual é o seu nome? ")
+
+## Primeira forma:
+print("Seja bem vindo ", nome, "!")
+print("Seja bem vindo " + nome + " !")
+
+## Segunda forma:
+print("Seja bem vindo {} !".format(nome))
+
+## Terceira forma:
+print(f"Seja bem vindo {nome} !")
+
+
+
