2ae23c073ed0816679607279e71f55cb1f5f1344
Rômulo Goleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2021-04-07 11:45:03 -0300

    create file desafio_007 and desafio_008
    novos desafios.

---
+++ C_E_V_Python/Desafios/desafio_007.py
@@ -0,0 +1,6 @@
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+m = (n1+n2)/2
+print('A média do aluno foi: {:.1f}'.format(m))#apenas uma casa decimal#
+print('A primeira nota foi {}, e a segunda nota foi {}'.format(n1,n2))
+
---
+++ C_E_V_Python/Desafios/desafio_008.py
@@ -0,0 +1,3 @@
+m = float(input('Informe uma dstância em metros: '))
+print('{:.0f} cm'.format(m*100))
+print('{:.0f} mm'.format(m*1000))
