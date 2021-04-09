88d510eb6d2bf50e8833a95973564759d2bfbec0
Rômulo Goleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2021-04-09 01:32:06 -0300

    Criado o desafio_009

---
+++ C_E_V_Python/Desafios/desafio_009.py
@@ -0,0 +1,16 @@
+# DESAFIO 009 - CRIANDO UMA TABUADA:
+
+n = float(input('Digite um número para ver a sua tabuada: '))
+print('='*10)
+print('{:.0f} x 1 = {:.0f}'.format(n, (n*1)))
+print('{:.0f} x 2 = {:.0f}'.format(n, (n*2)))
+print('{:.0f} x 3 = {:.0f}'.format(n, (n*3)))
+print('{:.0f} x 4 = {:.0f}'.format(n, (n*4)))
+print('{:.0f} x 5 = {:.0f}'.format(n, (n*5)))
+print('{:.0f} x 6 = {:.0f}'.format(n, (n*6)))
+print('{:.0f} x 7 = {:.0f}'.format(n, (n*7)))
+print('{:.0f} x 8 = {:.0f}'.format(n, (n*8)))
+print('{:.0f} x 9 = {:.0f}'.format(n, (n*9)))
+print('{:.0f} x 10 = {:.0f}'.format(n, (n*10)))
+
+print('='*10)
